af24f3d7c7cb92a45060c45ee78d0b84aa0ea410
Jason Chuang <jcchuang@cs.stanford.edu>
2013-09-09 18:51:31 -0700

    Added TypingUI.
    Moved TypingUI.js, TypingUI.html, TypingModel.js from development into master branch.

--- client_src/js/ElphabaModel.js
+++ client_src/js/ElphabaModel.js
@@ -4,7 +4,7 @@
 	 * Define a tree of attributes.
 	 * Track subtrees where the values of the attributes have been modified
 	 **/
-	var ElphabaModified = function() {
+	var ElphabaModel = function() {
 		this.attributes = {};
 	};
 	
@@ -13,7 +13,7 @@
 	 * @param {string} key Name of attribute.
 	 * @param {boolean} True if defined.
 	 **/
-	ElphabaModified.prototype.has = function( key ) {
+	ElphabaModel.prototype.has = function( key ) {
 		return this.attributes.hasOwnProperty( key );
 	};
 	
@@ -22,7 +22,7 @@
 	 * @param {string} key Name of the attribute.
 	 * @return {Object} Value of the attribute.
 	 **/
-	ElphabaModified.prototype.get = function( key ) {
+	ElphabaModel.prototype.get = function( key ) {
 		return this.attributes[ key ];
 	};
 	
@@ -31,7 +31,7 @@
 	 * @param {string} key Name of the attribute.
 	 * @param {Object} Value of the attribute.
 	 **/
-	ElphabaModified.prototype.set = function( key, value ) {
+	ElphabaModel.prototype.set = function( key, value ) {
 		this.attributes[ key ] = value;
 		return this;
 	};
@@ -40,7 +40,7 @@
 	 * Remove an attribute.
 	 * @param {string} key Name of the attribute.
 	 **/
-	ElphabaModified.prototype.unset = function( key ) {
+	ElphabaModel.prototype.unset = function( key ) {
 		delete this.attributes[ key ];
 		return this;
 	};
@@ -49,23 +49,22 @@
 	 * Stub function to maintain compatibility with backbone.
 	 * @private
 	 **/
-	ElphabaModified.prototype.trigger = function() {};
+	ElphabaModel.prototype.trigger = function() {};
 	
 	/**
-	 * Mark all attributes as unmodified.
+	 * Trigger a "modified" event and subsequently reset all attributes as un-modified.
 	 **/
-	ElphabaModified.prototype.flush = function() {
-		this.__prevMods__ = this.__currMods__ || {};
-		this.__currMods__ = {};
-		this.trigger( "modified", this.__prevMods__ );
+	ElphabaModel.prototype.flush = function() {
+		this.__previousModified__ = this.__modified__ || {};
+		this.__modified__ = {};
+		this.trigger( "modified", this.__previousModified__ );
 	};
 	
 	/**
 	 * Return true if "key" points to any node belonging to a modified subtree.
 	 * Return false otherwise.
-	 * @private
 	 **/
-	ElphabaModified.prototype.__isModified__ = function( modified, keys, depth ) {
+	var __isModified__ = function( modified, keys, depth ) {
 		if ( modified === true )
 			return true;
 		if ( depth === keys.length )
@@ -74,34 +73,34 @@
 		if ( Array.isArray( key ) ) {
 			for ( var i = 0; i < key.length; i++ )
 				if ( modified.hasOwnProperty( key[i] ) )
-					if ( this.__isModified__( modified[ key[i] ], keys, depth + 1 ) )
+					if ( __isModified__( modified[ key[i] ], keys, depth + 1 ) )
 						return true;
 			return false;
 		}
 		else if ( key === "*" ) {
 			for ( var k in modified )
-				if ( this.__isModified__( modified[k], keys, depth + 1 ) )
+				if ( __isModified__( modified[k], keys, depth + 1 ) )
 					return true;
 			return false;
 		}
 		else {
 			if ( modified.hasOwnProperty( key ) )
-				if ( this.__isModified__( modified[ key ], keys, depth + 1 ) )
+				if ( __isModified__( modified[ key ], keys, depth + 1 ) )
 					return true;
 			return false;
 		}
 	};
-	ElphabaModified.prototype.isModified = function() {
+	ElphabaModel.prototype.isModified = function() {
 		var keys = Array.prototype.slice.call( arguments );
-		if ( this.hasOwnProperty( "__currMods__" ) )
-			return this.__isModified__( this.__currMods__, keys, 0 );
+		if ( this.hasOwnProperty( "__modified__" ) )
+			return __isModified__( this.__modified__, keys, 0 );
 		else
 			return false;
 	};
-	ElphabaModified.prototype.wasModified = function() {
+	ElphabaModel.prototype.wasModified = function() {
 		var keys = Array.prototype.slice.call( arguments );
-		if ( this.hasOwnProperty( "__prevMods__" ) )
-			return this.__isModified__( this.__prevMods__, keys, 0 );
+		if ( this.hasOwnProperty( "__previousModified__" ) )
+			return __isModified__( this.__previousModified__, keys, 0 );
 		else
 			return false;
 	};
@@ -113,7 +112,7 @@
 	 * If multiple keys are defined or if a wildcard key is defined, return values are wrapped inside a hash object.
 	 * @private
 	 **/
-	ElphabaModified.prototype.__getModified__ = function( modified, keys, depth ) {
+	var __getModified__ = function( modified, keys, depth ) {
 		if ( modified === true )
 			return modified;
 		if ( depth === keys.length )
@@ -123,7 +122,7 @@
 			var results = {};
 			for ( var i = 0; i < key.length; i++ )
 				if ( modified.hasOwnProperty( key[i] ) )
-					results[ key[i] ] = this.__getModified__( modified[ key[i] ], keys, depth + 1 );
+					results[ key[i] ] = __getModified__( modified[ key[i] ], keys, depth + 1 );
 				else
 					results[ key[i] ] = undefined;
 			return results;
@@ -131,27 +130,27 @@
 		else if ( key === "*" ) {
 			var results = {};
 			for ( var k in modified )
-				results[ k ] = this.__getModified__( modified[ k ], keys, depth + 1 );
+				results[ k ] = __getModified__( modified[ k ], keys, depth + 1 );
 			return results;
 		}
 		else {
 			if ( modified.hasOwnProperty( key ) )
-				return this.__getModified__( modified[ key ], keys, depth + 1 );
+				return __getModified__( modified[ key ], keys, depth + 1 );
 			else
 				return undefined;
 		}
 	};
-	ElphabaModified.prototype.getModified = function() {
+	ElphabaModel.prototype.getModified = function() {
 		var keys = Array.prototype.slice.call( arguments );
-		if ( this.hasOwnProperty( "__currMods__" ) )
-			return this.__getModified__( this.__currMods__, keys, 0 );
+		if ( this.hasOwnProperty( "__modified__" ) )
+			return __getModified__( this.__modified__, keys, 0 );
 		else
 			return undefined;
 	};
-	ElphabaModified.prototype.gotModified = function() {
+	ElphabaModel.prototype.gotModified = function() {
 		var keys = Array.prototype.slice.call( arguments );
-		if ( this.hasOwnProperty( "__prevMods__" ) )
-			return this.__getModified__( this.__prevMods__, keys, 0 );
+		if ( this.hasOwnProperty( "__previousModified__" ) )
+			return __getModified__( this.__previousModified__, keys, 0 );
 		else
 			return undefined;
 	};
@@ -161,7 +160,7 @@
 	 * No wildcard keys allowed.
 	 * @private
 	 **/
-	ElphabaModified.prototype.__setModified__ = function( modified, keys, depth ) {
+	var __setModified__ = function( modified, keys, depth ) {
 		if ( modified === true )
 			return true;
 		if ( depth === keys.length )
@@ -169,23 +168,23 @@
 		var key = keys[ depth ];
 		if ( Array.isArray( key ) ) {
 			for ( var i = 0; i < key.length; i++ )
-				modified[ key[i] ] = this.__setModified__( modified[ key[i] ] || {}, keys, depth + 1 );
+				modified[ key[i] ] = __setModified__( modified[ key[i] ] || {}, keys, depth + 1 );
 			return modified;
 		}
 		else if ( key === "*" ) {
 			return true;
 		}
 		else {
-			modified[ key ] = this.__setModified__( modified[ key ] || {}, keys, depth + 1 );
+			modified[ key ] = __setModified__( modified[ key ] || {}, keys, depth + 1 );
 			return modified;
 		}
 	};
-	ElphabaModified.prototype.setModified = function() {
+	ElphabaModel.prototype.setModified = function() {
 		var keys = Array.prototype.slice.call( arguments );
-		this.__currMods__ = this.__setModified__( this.__currMods__ || {}, keys, 0 );
+		this.__modified__ = __setModified__( this.__modified__ || {}, keys, 0 );
 	};
 
-	ElphabaModified.prototype.__getAttribute__ = function( attribute, keys, depth ) {
+	var __getAttribute__ = function( attribute, keys, depth ) {
 		var key = keys[ depth ];
 		if ( Array.isArray( key ) ) {
 			var results = {};
@@ -198,7 +197,7 @@
 			else
 				for ( var i = 0; i < key.length; i++ )
 					if ( attribute.hasOwnProperty( key[i] ) )
-						results[ key[i] ] = this.__getAttribute__( attribute[ key[i] ], keys, depth + 1 );
+						results[ key[i] ] = __getAttribute__( attribute[ key[i] ], keys, depth + 1 );
 					else
 						results[ key[i] ] = undefined;
 			return results;
@@ -210,7 +209,7 @@
 					results[ k ] = attribute[ k ];
 			else
 				for ( var k in attribute )
-					results[ k ] = this.__getAttribute__( attribute[ k ], keys, depth + 1 );
+					results[ k ] = __getAttribute__( attribute[ k ], keys, depth + 1 );
 			return results;
 		}
 		else {
@@ -221,12 +220,12 @@
 					return undefined;
 			else
 				if ( attribute.hasOwnProperty( key ) )
-					return this.__getAttribute__( attribute[ key ], keys, depth + 1 );
+					return __getAttribute__( attribute[ key ], keys, depth + 1 );
 				else
 					return undefined;
 		}
 	};
-	ElphabaModified.prototype.getAttribute = function() {
+	ElphabaModel.prototype.getAttribute = function() {
 		var keys = Array.prototype.slice.call( arguments );
 		var key = keys[ 0 ];
 		if ( Array.isArray( key ) ) {
@@ -240,7 +239,7 @@
 			else
 				for ( var i = 0; i < key.length; i++ )
 					if ( this.has( key[i] ) )
-						results[ key[i] ] = this.__getAttribute__( this.get( key[i] ), keys, 1 );
+						results[ key[i] ] = __getAttribute__( this.get( key[i] ), keys, 1 );
 					else
 						results[ key[i] ] = undefined;
 			return results;
@@ -252,7 +251,7 @@
 					results[ k ] = this.get( k );
 			else
 				for ( var k in this.attributes )
-					results[ k ] = this.__getAttribute__( this.get( k ), keys, 1 );
+					results[ k ] = __getAttribute__( this.get( k ), keys, 1 );
 			return results;
 		}
 		else {
@@ -263,13 +262,13 @@
 					return undefined;
 			else
 				if ( this.has( key ) )
-					return this.__getAttribute__( this.get( key ), keys, 1 );
+					return __getAttribute__( this.get( key ), keys, 1 );
 				else
 					return undefined
 		}
 	};
 	
-	ElphabaModified.prototype.__setAttribute__ = function( attribute, keys, depth, value ) {
+	var __setAttribute__ = function( attribute, keys, depth, value ) {
 		var key = keys[ depth ];
 		if ( Array.isArray( key ) ) {
 			if ( depth + 1 === keys.length )
@@ -281,9 +280,9 @@
 			else
 				for ( var i = 0; i < key.length; i++ )
 					if ( attribute.hasOwnProperty( key[i] ) )
-						attribute[ key[i] ] = this.__setAttribute__( attribute[ key[i] ], keys, depth + 1, value[i] );
+						attribute[ key[i] ] = __setAttribute____( attribute[ key[i] ], keys, depth + 1, value[i] );
 					else
-						attribute[ key[i] ] = this.__setAttribute__( {}, keys, depth + 1, value[i] );
+						attribute[ key[i] ] = __setAttribute____( {}, keys, depth + 1, value[i] );
 		}
 		else if ( key === "*" ) {
 			if ( depth + 1 == keys.length )
@@ -294,7 +293,7 @@
 						attribute[ k ] = value;
 			else
 				for ( var k in attribute )
-					attribute[ k ] = this.__setAttribute( attribute[ k ], keys, depth + 1, value );
+					attribute[ k ] = __setAttribute____( attribute[ k ], keys, depth + 1, value );
 		}
 		else {
 			if ( depth + 1 === keys.length )
@@ -304,13 +303,13 @@
 					attribute[ key ] = value;
 			else
 				if ( attribute.hasOwnProperty( key ) )
-					attribute[ key ] = this.__setAttribute__( attribute[ key ], keys, depth + 1, value );
+					attribute[ key ] = __setAttribute____( attribute[ key ], keys, depth + 1, value );
 				else
-					attribute[ key ] = this.__setAttribute__( {}, keys, depth + 1, value );
+					attribute[ key ] = __setAttribute____( {}, keys, depth + 1, value );
 		}
 		return attribute;
 	};
-	ElphabaModified.prototype.setAttribute = function() {
+	ElphabaModel.prototype.setAttribute = function() {
 		var keys = Array.prototype.slice.call( arguments );
 		var value = keys.pop();
 		var key = keys[ 0 ];
@@ -318,70 +317,63 @@
 			if ( 1 === keys.length )
 				for ( var i = 0; i < key.length; i++ )
 					if ( value[i] === undefined )
-						this.unset( key[i], { "silent" : true } );
+						this.unset( key[i], { "silent" : true } );          // Compatible with backbone models
 					else
-						this.set( key[i], value[i], { "silent" : true } );
+						this.set( key[i], value[i], { "silent" : true } );  // Compatible with backbone models
 			else
 				for ( var i = 0; i < key.length; i++ )
-					this.set( key[i], this.__setAttribute__( this.get( key[i] ) || {}, keys, 1, value[i] ), { "silent" : true } );
-			this.trigger( "change " + key.map(function(d) { return "change:"+d } ).join(" ") );
+					this.set( key[i], __setAttribute____( this.get( key[i] ) || {}, keys, 1, value[i] ), { "silent" : true } );  // Compatible with backbone models
+			this.trigger( "change " + key.map(function(d) { return "change:"+d } ).join(" ") );                                  // Compatible with backbone models
 		}
 		else if ( key === "*" ) {
 			if ( 1 === keys.length )
 				for ( var k in this.attributes )
 					if ( value === undefined )
-						this.unset( k, { "silent" : true } );
+						this.unset( k, { "silent" : true } );       // Compatible with backbone models
 					else
-						this.set( k, value, { "silent" : true } );
+						this.set( k, value, { "silent" : true } );  // Compatible with backbone models
 			else
 				for ( var k in this.attributes )
-					this.set( k, this.__setAttribute__( this.get( k ) || {}, keys, 1, value ), { "silent" : true } );
-			this.trigger( "change " + _.keys(this.attributes).map(function(d) { return "change:"+d } ).join(" ") );
+					this.set( k, __setAttribute____( this.get( k ) || {}, keys, 1, value ), { "silent" : true } );   // Compatible with backbone models
+			this.trigger( "change " + _.keys(this.attributes).map(function(d) { return "change:"+d } ).join(" ") );  // Compatible with backbone models
 		}
 		else {
 			if ( 1 === keys.length )
 				if ( value === undefined )
-					this.unset( key, { "silent" : true } );
+					this.unset( key, { "silent" : true } );        // Compatible with backbone models
 				else
-					this.set( key, value, { "silent" : true } );
+					this.set( key, value, { "silent" : true } );   // Compatible with backbone models
 			else
-				this.set( key, this.__setAttribute__( this.get( key ) || {}, keys, 1, value ), { "silent" : true } );
-			this.trigger( "change change:" + key );
+				this.set( key, __setAttribute____( this.get( key ) || {}, keys, 1, value ), { "silent" : true } );  // Compatible with backbone models
+			this.trigger( "change change:" + key );                                                                 // Compatible with backbone models
 		}
 		return this;
 	};
 
-	ElphabaModified.prototype.setAttributeAndModified = function() {
+	ElphabaModel.prototype.setAttributeAndModified = function() {
 		var keys = Array.prototype.slice.call( arguments );
 		var value = keys.pop();
-		ElphabaModified.prototype.setModified.apply( this, keys );
-		ElphabaModified.prototype.setAttribute.apply( this, arguments );
+		ElphabaModel.prototype.setModified.apply( this, keys );
+		ElphabaModel.prototype.setAttribute.apply( this, arguments );
 		return this;
 	};
 
 	// Extend backbone model
-	Backbone.Model.prototype.flush = ElphabaModified.prototype.flush;
+	Backbone.Model.prototype.flush = ElphabaModel.prototype.flush;
 
-	Backbone.Model.prototype.__isModified__  = ElphabaModified.prototype.__isModified__ ;
-	Backbone.Model.prototype.__getModified__ = ElphabaModified.prototype.__getModified__;
-	Backbone.Model.prototype.__setModified__ = ElphabaModified.prototype.__setModified__;
+	Backbone.Model.prototype.isModified = ElphabaModel.prototype.isModified;
+	Backbone.Model.prototype.wasModified = ElphabaModel.prototype.wasModified;
+	Backbone.Model.prototype.getModified = ElphabaModel.prototype.getModified;
+	Backbone.Model.prototype.gotModified = ElphabaModel.prototype.gotModified;
+	Backbone.Model.prototype.setModified = ElphabaModel.prototype.setModified;
 
-	Backbone.Model.prototype.isModified = ElphabaModified.prototype.isModified;
-	Backbone.Model.prototype.wasModified = ElphabaModified.prototype.wasModified;
-	Backbone.Model.prototype.getModified = ElphabaModified.prototype.getModified;
-	Backbone.Model.prototype.gotModified = ElphabaModified.prototype.gotModified;
-	Backbone.Model.prototype.setModified = ElphabaModified.prototype.setModified;
+	Backbone.Model.prototype.getAttr = ElphabaModel.prototype.getAttribute;
+	Backbone.Model.prototype.setAttr = ElphabaModel.prototype.setAttributeAndModified;
 
-	Backbone.Model.prototype.__getAttribute__ = ElphabaModified.prototype.__getAttribute__;
-	Backbone.Model.prototype.__setAttribute__ = ElphabaModified.prototype.__setAttribute__;
-
-	Backbone.Model.prototype.getAttr = ElphabaModified.prototype.getAttribute;
-	Backbone.Model.prototype.setAttr = ElphabaModified.prototype.setAttributeAndModified;
-
-	// Export (node.js) or declare in global namespace
+	// Export (node.js) or declare in global namespace (browser)
 	if ( ! exports.hasOwnProperty( "Elphaba" ) ) {
 		exports[ "Elphaba" ] = {};
 	}
-	exports[ "Elphaba" ].Modified = ElphabaModified;
+	exports[ "Elphaba" ].Model = ElphabaModel;
 
 })( typeof module === "undefined" ? this : module.exports );
