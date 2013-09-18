15c53adfad6338fe35ca2299357fd5204e48e511
Jason Chuang <jcchuang@cs.stanford.edu>
2013-09-18 02:47:06 -0700

    Added comments.

--- client_src/js/ptm.js
+++ client_src/js/ptm.js
@@ -11,16 +11,23 @@ PTM.prototype.run = function() {
 	this.typingUI = new TypingUI( { "model" : this.typingModel } );
 	this.server = new TranslateServer();
 //	this.server.SERVER_URL = "http://localhost:8888/cgi-bin/redirect.py"
-	this.typingState.on( "syncTranslation", this.onSyncTranslation.bind(this) );
+	this.typingState.on( "syncTranslation", this.updateTranslation.bind(this) );
 };
 
+/**
+ * Initialize TypingUI.
+ **/
 PTM.prototype.initTranslation = function() {
 	var handler = function( translation ) { this.typingState.updateTranslation( translation ) };
 	var sourceText = this.sourceBox.getSourceText();
 	this.server.translate( handler.bind(this), sourceText );
 };
 
-PTM.prototype.onSyncTranslation = function( syncKey ) {
+/**
+ * Update TypingUI.
+ * @param {Object} syncKey An arbitrary identifier that is unique for each request. Pass back to TypingUI to ensure synchronization.
+ **/
+PTM.prototype.updateTranslation = function( syncKey ) {
 	var handler = function( translation ) { this.typingState.syncTranslation( syncKey, translation ) };
 	var sourceText = this.sourceBox.getSourceText();
 	var targetPrefix = this.typingState.getUserText();
