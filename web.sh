5366655c7508b0e4167fd279bb4d907b5d6f9c3b
Jason Chuang <jcchuang@cs.stanford.edu>
2014-03-18 21:45:40 -0700

    Updated web.sh to automatically launch Chrome after server is started.

--- web.sh
+++ web.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
 
-#python -m SimpleHTTPServer 8888
-python -m CGIHTTPServer 8888
+launch_browser() {
+	OS=${OSTYPE//[0-9.]/}
+	if [ "$OS" == "darwin" ]
+	then
+		sleep 2
+		echo
+		echo "Opening http://localhost:8888 in Google Chrome..."
+		echo
+		open -a Google\ Chrome "http://localhost:8888/client_src/index.html"
+	else
+		echo "TranslateUI is now available at http://localhost:8888"
+	fi
+}
+
+launch_localhost() {
+	echo
+	echo "Starting a python CGIHTTPServer at port 8888..."
+	echo "Press Ctrl+C to close this python web server."
+	echo
+	python -m CGIHTTPServer 8888
+}
+
+launch_browser &
+launch_localhost
